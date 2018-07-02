4c4b56215ad070e62f1d1c32a50b041a9e988841
Matthew Dorner <36939751+MatthewDorner@users.noreply.github.com>
2018-07-01 21:40:04 -0500

    Update entrypoint.sh
    Copy instead of move.

--- nginx/conf/entrypoint.sh
+++ nginx/conf/entrypoint.sh
@@ -2,7 +2,7 @@
 rm /etc/nginx/conf.d/*.conf
 if [ "$ENCRYPTION_TYPE" = "auto" ]; then
 	echo "ENCRYPTION_TYPE was auto, use a cert from letsencrypt" \
-	&& mv /etc/nginx/conf.d/defaultautossl /etc/nginx/conf.d/defaultautossl.conf \
+	&& cp /etc/nginx/conf.d/defaultautossl /etc/nginx/conf.d/defaultautossl.conf \
 	&& (crontab -l 2>/dev/null; echo "30 2 * * 1 /usr/bin/certbot-auto renew --quiet --no-self-upgrade >> /var/log/letsencrypt/le-renew.log") | crontab - \
 	&& mkdir -p /var/log/letsencrypt && touch /var/log/letsencrypt/install.log \
 	&& if [ ! -f /etc/letsencrypt/live/${DOMAIN_NAME}/cert.pem ]; then
@@ -12,9 +12,9 @@ if [ "$ENCRYPTION_TYPE" = "auto" ]; then
     && nginx -s reload
 elif [ "$ENCRYPTION_TYPE" = "self" ]; then
     echo "ENCRYPTION_TYPE was self, use the provided ssl cert" \
-    && mv /etc/nginx/conf.d/defaultselfssl /etc/nginx/conf.d/defaultselfssl.conf
+    && cp /etc/nginx/conf.d/defaultselfssl /etc/nginx/conf.d/defaultselfssl.conf
 elif [ "$ENCRYPTION_TYPE" = "none" ]; then
 	echo "ENCRYPTION_TYPE was none, use http only" \
-   	&& mv /etc/nginx/conf.d/default /etc/nginx/conf.d/default.conf
+   	&& cp /etc/nginx/conf.d/default /etc/nginx/conf.d/default.conf
 fi
 nginx -g "daemon off;"
